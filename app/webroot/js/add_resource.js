d4b4d082d32b3f4733e7c1ed432b282b1a0f76dc
Jason Galuten <jason@galuten.com>
2011-07-30 23:37:04 -0700

    return in last field submits form like normal

--- app/webroot/js/add_resource.js
+++ app/webroot/js/add_resource.js
@@ -1,16 +1,21 @@
 $(document).ready(function(){  
 	$(':input').keypress(function(e) {
 		if (e.which == 13) {	
+			var goAhead = false;
 			var queueItem = $(this).parent('.uploadifyQueueItem');
 			if (queueItem.hasClass('completed')) {
-				var url = '/resources/saveDescription';
-				var data = {'id' : $(this).attr('rel'), 'description' : $(this).val()};
-				$.get(url, data, function() {
-					queueItem.next('.uploadifyQueueItem').find(':input').focus();
-					queueItem.remove();
-				});
+				if ($('.uploadifyQueueItem').length == 1) { // if it's the last one, submit the form like normal!
+					goAhead = true;
+				} else {
+					var url = '/resources/saveDescription';
+					var data = {'id' : $(this).attr('rel'), 'description' : $(this).val()};
+					$.get(url, data, function() {
+						queueItem.next('.uploadifyQueueItem').find(':input').focus();
+						queueItem.remove();
+					});
+				}
 			} 
-			e.preventDefault();
+			if (!goAhead) e.preventDefault();
 		}
 	});
 });
